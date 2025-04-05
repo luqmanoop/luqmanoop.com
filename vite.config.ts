cb6a25f8893605f6c9fe3916fcfdff36d91f6d5a
Luqman Olushi <olushilukmon03@gmail.com>
2025-04-05 21:24:02 +0100

    chore: add mdx remar plugins - frontmatter

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,15 @@ import mdx from "@mdx-js/rollup";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import remarkFrontmatter from "remark-frontmatter";
+import remarkMdxFrontmatter from "remark-mdx-frontmatter";
 
 export default defineConfig({
   plugins: [
     mdx({
       jsxImportSource: "react",
       providerImportSource: "@mdx-js/react",
-      
+      remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
     }),
     tailwindcss(),
     reactRouter(),
