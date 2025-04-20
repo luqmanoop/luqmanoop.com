19c10e4b13e6b00edc34c072b1efd2a13fcd0ab0
Luqman Olushi <olushilukmon03@gmail.com>
2025-04-20 04:12:49 +0100

    feat: update navigation for mobile

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,6 +1,6 @@
 import { Menu } from "lucide-react";
-import { useState } from "react";
-import { NavLink } from "react-router";
+import { useEffect, useState } from "react";
+import { NavLink, useNavigation } from "react-router";
 
 import { ThemeToggle } from "./ThemeToggle";
 
@@ -40,71 +40,73 @@ const pagesLinks = [
 ];
 
 export const NavBar = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
+  const onToggleNavigation = () => {
+    document.body.classList.toggle("overflow-hidden");
+    document.getElementById("navigation")?.classList.toggle("hidden");
+  };
+
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    if (navigation.state !== "loading") return;
+
+    document.body.classList.remove("overflow-hidden");
+    const navigationElement = document.getElementById("navigation");
+    
+    if (!navigationElement || navigationElement.classList.contains("hidden")) return;
+    
+    navigationElement.classList.add("hidden");
+  }, [navigation.state]);
 
   return (
-    <header className="sticky top-0 z-50">
-      <nav className="text-xl md:text-base">
-        <div className="shadow dark:shadow-none dark:border-b dark:border-slate-900 bg-white dark:bg-slate-950">
-          <div className="flex max-w-6xl p-4 md:py-0 mx-auto items-center justify-center">
-            <div className="flex-1 items-center justify-center">
-              <NavLink to="/" viewTransition className="font-bold">
-                <img
-                  src="/assets/logo-light.png"
-                  className="w-1/4 md:w-[5.4rem] dark:hidden"
-                  alt="luqmanoop logo"
-                />
-                <img
-                  src="/assets/logo-dark.png"
-                  alt="luqmanoop logo"
-                  className="w-1/4 md:w-[5.4rem] hidden dark:block"
-                />
-              </NavLink>
-            </div>
-            <div className="space-x-8 hidden md:flex font-medium px-12 py-4 items-center">
-              {pagesLinks.map((link) => (
-                <Link
-                  key={link.to}
-                  to={link.to}
-                  label={link.label}
-                  onClick={() => setMenuOpen(false)}
-                />
-              ))}
+    <header className="sticky top-0 z-50 shadow dark:shadow-none dark:border-b dark:border-slate-900 bg-white dark:bg-slate-950">
+      <div className="flex flex-col md:flex-row max-w-6xl md:px-6 md:py-4 mx-auto">
+        <div className="flex-1 flex justify-center items-center md:justify-start shadow md:shadow-none px-6 py-3 md:p-0">
+          <NavLink to="/" viewTransition>
+            <img
+              src="/assets/logo-light.png"
+              className="w-1/4 md:w-[5.4rem] dark:hidden"
+              alt="luqmanoop logo"
+            />
+            <img
+              src="/assets/logo-dark.png"
+              alt="luqmanoop logo"
+              className="w-1/4 md:w-[5.4rem] hidden dark:block"
+            />
+          </NavLink>
 
-              <ThemeToggle />
-            </div>
+          <div className="md:hidden flex items-center gap-4 md:gap-2">
+            <ThemeToggle />
 
-            <div className="md:hidden flex items-center gap-2">
-              <ThemeToggle />
-
-              <button
-                role="button"
-                aria-label="Toggle menu"
-                onClick={() => setMenuOpen(!menuOpen)}
-                className="p-2 focus:ring-gray-300 focus:ring-4 focus:outline-none rounded-md focus:dark:ring-gray-700"
-              >
-                <Menu className="h-6 w-6" />
-              </button>
-            </div>
+            <button
+              role="button"
+              aria-label="Toggle menu"
+              onClick={() => onToggleNavigation()}
+              className="p-2 focus:ring-gray-300 focus:ring-4 focus:outline-none rounded-md focus:dark:ring-gray-700"
+            >
+              <Menu className="h-6 w-6" />
+            </button>
           </div>
         </div>
-        {menuOpen && (
-          <div className="relative h-screen">
-            <div className="absolute bottom-0 border-t border-gray-200 dark:border-gray-800 right-0 left-0 w-full h-full bg-white dark:bg-slate-950">
-              <div className="space-y-6 flex flex-col font-medium px-4 py-4 h-full">
-                {pagesLinks.map((link) => (
-                  <Link
-                    key={link.to}
-                    to={link.to}
-                    label={link.label}
-                    onClick={() => setMenuOpen(false)}
-                  />
-                ))}
-              </div>
-            </div>
-          </div>
-        )}
-      </nav>
+
+        <nav
+          className="items-center w-full md:w-auto hidden md:flex h-screen md:h-auto"
+          id="navigation"
+          aria-label="Navigation"
+        >
+          <ul className="md:space-x-8 space-y-8 md:space-y-0 px-6 py-6 md:p-0 md:flex font-medium md:items-center text-lg md:text-base">
+            {pagesLinks.map((link) => (
+              <li key={link.to} className="md:inline-flex">
+                <Link to={link.to} label={link.label} />
+              </li>
+            ))}
+
+            <li className="hidden md:inline-flex">
+              <ThemeToggle />
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
